a319eb49ac9a0132a05300e7ddbb7867ce9fb6c5
Guilherme De Napoli <guinaps@gmail.com>
2012-01-18 13:50:26 -0200

    Data plotting code for exercise 1

--- mlclass-ex1/plotData.m
+++ mlclass-ex1/plotData.m
@@ -16,10 +16,9 @@ function plotData(x, y)
 
 figure; % open a new figure window
 
-
-
-
-
+plot(x, y, 'rx', 'MarkerSize', 10);
+ylabel('Profit in $10,000s');
+xlabel('Population of the City in 10,000s');
 
 % ============================================================
 
