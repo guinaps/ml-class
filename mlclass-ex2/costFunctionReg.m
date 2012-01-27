ea76a8762abcde6c1cafd04162f02a923291597c
Guilherme De Napoli <guinaps@gmail.com>
2012-01-27 02:28:44 -0200

    Completed ex. 2 cost and gradient for regularized logistic regression

--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -17,10 +17,15 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X * theta);
+reg_theta = theta;
+reg_theta(1) = 0;
 
+J = (- (y' * log(h) + (1-y)' * log(1-h)) / m +
+    lambda / (2*m) * reg_theta' * reg_theta);
 
-
-
+grad = (X' * (h - y) / m +
+       lambda / m * reg_theta);
 
 % =============================================================
 
