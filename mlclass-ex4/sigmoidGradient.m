06156f41245e8ab09dbc8a83f9d1aa5f077b5682
Guilherme De Napoli <guinaps@gmail.com>
2012-02-09 12:32:43 -0200

    Completed ex. 4 sigmoid gradient

--- mlclass-ex4/sigmoidGradient.m
+++ mlclass-ex4/sigmoidGradient.m
@@ -12,18 +12,8 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
-
-
-
-
-
-
-
-
-
-
-
-
+s = sigmoid(z);
+g = s .* (1 - s);
 
 % =============================================================
 
